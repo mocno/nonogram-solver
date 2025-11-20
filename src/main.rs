f5254f64c03374fca8ae3dcdfd509f246060b413
Gustavo Correa <gustavo.correa.mogi@gmail.com>
2025-11-20 17:13:58 -0300

    Adicionar CLI para testar completude

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,52 @@
 use std::time::Instant;
 
-use rand::{Rng, SeedableRng, rngs::StdRng};
+use clap::Parser;
 
-use crate::nonogram::{Board, Column, PaintedBoard, PaintedColumn};
+use crate::results::get_mean_rate_painting_nonogram_board;
 
 mod nonogram;
+mod results;
 
-fn get_mean_rate_painting_columns(
-    rng: &mut impl Rng,
+/// CLI simples para testas completude de nonogramas de diferentes tamanhos e preenchimento
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long, default_value_t = 15)]
     size: usize,
-    p: f64,
-    q: f64,
+
+    #[arg(short = 'n', long, default_value_t = 50)]
     num_test: usize,
-) -> Result<f32, ()> {
-    let mut sum = 0.0;
 
-    for _ in 0..num_test {
-        let painted_column = PaintedColumn::new_random(rng, p, size);
-        let info = painted_column.get_info();
-        let mut column = Column::new_ramdom_from(&painted_column, rng, q);
-        let new_column = column.try_fit(&info).unwrap();
+    #[arg(short = 'p', long, default_value_t = 200)]
+    num_p_tests: usize,
 
-        sum += new_column.painted_rate();
-
-        if !column.verify(painted_column) {
-            return Err(());
-        }
-    }
-
-    Ok(sum / (num_test as f32))
+    #[arg(short = 't', long, default_value_t = false)]
+    time: bool,
 }
 
-fn get_mean_rate_painting_nonogram_board(
-    rng: &mut impl Rng,
-    size: usize,
-    p: f64,
-    num_test: usize,
-) -> Result<f32, ()> {
-    let mut sum = 0.0;
-
-    for _ in 0..num_test {
-        let painted_board: PaintedBoard = PaintedBoard::new_random(rng, size, size, p);
-        let mut board: Board = painted_board.into_empty_board();
-
-        board.try_paint();
-
-        sum += board.painted_rate();
-
-        if !board.verify(painted_board) {
-            return Err(());
-        }
+fn print_table_pc(size: usize, num_test: usize, num_p_tests: usize) -> Result<(), ()> {
+    let mut rng = rand::rng();
+    println!("p;c");
+    let ps = (0..=num_p_tests).map(|v| v as f64 / num_p_tests as f64);
+    for p in ps {
+        let c = get_mean_rate_painting_nonogram_board(&mut rng, size, p, num_test)?;
+        println!("{p};{c}");
     }
-
-    Ok(sum / num_test as f32)
+    Ok(())
 }
 
-fn main() {
-    let mut rng = StdRng::seed_from_u64(9);
-    let now = Instant::now();
-    match get_mean_rate_painting_columns(&mut rng, 15, 0.5, 0.2, 200) {
-        Ok(mean) => println!("Média da completude: {:.2}%", 100.0 * mean),
-        Err(_) => println!("Deu errado no testes da média"),
+fn main() -> Result<(), ()> {
+    let args = Args::parse();
+
+    if args.time {
+        let now = Instant::now();
+        let result = print_table_pc(args.size, args.num_test, args.num_p_tests);
+        let elapsed_time = now.elapsed();
+        println!("Time: {:} seconds.", elapsed_time.as_secs());
+
+        result
+    } else {
+        print_table_pc(args.size, args.num_test, args.num_p_tests)
     }
-    match get_mean_rate_painting_nonogram_board(&mut rng, 15, 0.5, 200) {
-        Ok(mean) => println!("Média da completude: {:.2}%", 100.0 * mean),
-        Err(_) => println!("Deu errado no testes da média"),
-    }
-    let elapsed_time = now.elapsed();
-    println!("Time: {:} seconds.", elapsed_time.as_secs());
 }
